dd52bc79f339c1ba9ed046face7b760ce7350cf4
CHEN YIWEI <22256024@life.hkbu.edu.hk>
2026-01-14 12:43:39 +0800

    Fix ChromaDB collection already exists error

--- tradingagents/agents/utils/memory.py
+++ tradingagents/agents/utils/memory.py
@@ -20,12 +20,20 @@ class FinancialSituationMemory:
             self.embedding = "nomic-embed-text"
             self.client = OpenAI(base_url=config["backend_url"])
             self.chroma_client = chromadb.Client(Settings(allow_reset=True))
-            self.situation_collection = self.chroma_client.create_collection(name=name)
+            # Get or create collection (handle existing collections)
+            try:
+                self.situation_collection = self.chroma_client.get_collection(name=name)
+            except:
+                self.situation_collection = self.chroma_client.create_collection(name=name)
         else:
             self.embedding = "text-embedding-3-small"
             self.client = OpenAI(base_url=config["backend_url"])
             self.chroma_client = chromadb.Client(Settings(allow_reset=True))
-            self.situation_collection = self.chroma_client.create_collection(name=name)
+            # Get or create collection (handle existing collections)
+            try:
+                self.situation_collection = self.chroma_client.get_collection(name=name)
+            except:
+                self.situation_collection = self.chroma_client.create_collection(name=name)
 
     def get_embedding(self, text):
         """Get OpenAI embedding for a text"""
